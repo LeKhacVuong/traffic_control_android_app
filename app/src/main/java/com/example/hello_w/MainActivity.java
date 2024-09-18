b559c2cad0c258180e087b2dc818ca0ee715a514
Lê Khắc Vương <132425535+LeKhacVuong@users.noreply.github.com>
2024-09-18 07:55:40 +0700

    init
    init

---
+++ app/src/main/java/com/example/hello_w/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.hello_w;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton imgBtnSN;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imgBtnSN = findViewById(R.id.img_btn_sn);
+
+        imgBtnSN.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+
+    }
+}
